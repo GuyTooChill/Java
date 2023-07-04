25cf1a8477565b6ef7923974947dc68d750b09d1
Diego G <Diego93gonzo@gmail.com>
2023-07-04 18:45:18 -0400

    new

---
+++ Week_1/greetings/FirstJava.java
@@ -0,0 +1,7 @@
+class FirstJava{
+    public static void main(String[] args){
+        System.out.println("My name is Diego");
+        System.out.println("I am 29 years old.");
+        System.out.println("My hometown is Delray Beach, FL.");
+    }
+}
